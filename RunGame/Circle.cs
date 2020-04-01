5a8e63e278d8bd9c1a195369f6ea948da1facfc7
Виталий <spidometrus@outlook.com>
2020-04-01 17:30:50 +0800

    lesson 4

---
+++ RunGame/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunGame
+{
+    class Circle
+    {
+        public Point center { get; private set; }
+        public int radius { get; private set; }
+        public Color color { get; private set; }
+        public Circle(int x, int y, int radius)
+        {
+            center = new Point(x, y);
+            this.radius = radius;
+            color = Color.Blue;
+        }
+    }
+}
